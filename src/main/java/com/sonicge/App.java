b05fcde0f3c9248fadf490f2ff0f6fe513e2f6c1
etge <etge@iflytek.com>
2025-12-17 15:58:25 +0800

    initial commit: java-playground项目初始化

---
+++ src/main/java/com/sonicge/App.java
@@ -0,0 +1,13 @@
+package com.sonicge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
