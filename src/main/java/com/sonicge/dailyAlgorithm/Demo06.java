dee145ee2e4a830f88d7d3b8d4507f30c1c80e45
get <249628024@qq.com>
2026-01-23 22:15:54 +0800

    feat(daily-algorithm):每日一题(1.23)，跳跃游戏II

---
+++ src/main/java/com/sonicge/dailyAlgorithm/Demo06.java
@@ -0,0 +1,58 @@
+package com.sonicge.dailyAlgorithm;
+
+import jdk.internal.org.objectweb.asm.tree.JumpInsnNode;
+
+import java.time.temporal.JulianFields;
+import java.util.Scanner;
+
+/**
+ * 给定一个长度为 n 的 0 索引整数数组 nums。初始位置在下标 0。
+ * 每个元素 nums[i] 表示从索引 i 向后跳转的最大长度。换句话说，如果你在索引 i 处，你可以跳转到任意 (i + j) 处：
+ * 0 <= j <= nums[i] 且
+ * i + j < n
+ * 返回到达 n - 1 的最小跳跃次数。测试用例保证可以到达 n - 1。
+ * 输入: nums = [2,3,1,1,4]
+ * 输出: 2
+ * 解释: 跳到最后一个位置的最小跳跃数是 2。
+ *      从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * @Author: sonicge
+ * @CreateTime: 2026-01-23
+ *
+ */
+
+public class Demo06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int jump = jump(nums);
+        System.out.println("跳跃到末尾，需要"+jump+"步");
+
+
+    }
+    public static int jump(int[] nums){
+        int size=nums.length;
+        if(size == 1)return 0;
+        int cur=0;
+        int res=0;
+        int maxRange=0;
+
+        for(int i=0;i<size;i++){
+            maxRange = Math.max(maxRange,i+nums[i]);
+            //如果当前可以到达的最远距离可以到达末尾的话，直接跳了，不用在范围里面再找最远值了
+            if(maxRange >=size-1){
+                res++;
+                return res;
+            }
+            if(cur == i){
+                cur = maxRange;
+                res++;
+            }
+        }
+        return res;
+    }
+}
