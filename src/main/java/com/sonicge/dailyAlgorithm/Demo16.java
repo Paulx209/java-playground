d7d1a6bc054e9e779566145b87843b4da88f7ae0
get <249628024@qq.com>
2026-02-15 07:42:33 +0800

    feat(daily-algorithm):每日一题(2.15)，最长公共前缀

---
+++ src/main/java/com/sonicge/dailyAlgorithm/Demo16.java
@@ -0,0 +1,50 @@
+package com.sonicge.dailyAlgorithm;
+
+
+import java.util.Scanner;
+
+/**
+ * 最长公共前缀
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 输入：strs = ["flower","flow","flight"]
+ * 输出："fl"
+ * @Author: sonicge
+ * @CreateTime: 2026-02-15
+ */
+
+public class Demo16 {
+    public static void main(String[] args) {
+        //1.初始化
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        String[] strs = new String[size];
+        for(int i=0;i<size;i++){
+            strs[i] = sc.next();
+        }
+        //2.调用处理函数
+        String commonPrefix = longestCommonPrefix(strs);
+        System.out.println("共同的字符串前缀为:"+commonPrefix);
+
+    }
+    public static String longestCommonPrefix (String[] args){
+        String str = args[0];
+        int res = 0;
+        for(int i=0;i<str.length();i++){
+            //截取一部分
+            String substring = str.substring(0, i + 1);
+            boolean flag = isSimilar(args,substring);
+            if(!flag) return str.substring(0,res);
+            res = i+1;
+        }
+        return str.substring(0,res);
+    }
+
+    public static boolean isSimilar(String[] args,String subString){
+        for(String str:args){
+            boolean b = str.startsWith(subString);
+            if(!b)return false;
+        }
+        return true;
+    }
+}
